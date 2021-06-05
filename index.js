2125fa1ea0b666f7b79a6ba550b2a01219c9c008
Nawiss8 <85230219+Nawiss8@users.noreply.github.com>
2021-06-05 14:59:41 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -62,11 +62,8 @@ client.on("message",message => {
       message.reply("https://www.youtube.com/channel/UCWr0...");
       
 
-  }
-  
-} 
-
-)
+  } 
+} );
 
 
 client.on("message",message => {
@@ -78,7 +75,7 @@ client.on("message",message => {
       
   }  
 } 
-)
+);
 
 client.on("message",message => {
   if(message.author.bot) return;
@@ -89,7 +86,7 @@ client.on("message",message => {
       
   }  
 } 
-)
+);
 
 client.on("message",message => {
   if(message.author.bot) return;
@@ -100,17 +97,17 @@ client.on("message",message => {
       
   }  
 } 
-)
+);
 client.on("message",message => {
   if(message.author.bot) return;
 
-  //!Great
-  if(message.content == prefix + "Great"){
-      message.reply("a finir^^");
+  //!lisez bien les regles
+  if(message.content == prefix + "lisez bien les regles"){
+      message.reply("oui s'il vous plait,et passez un bon moment peut importe si c'est le matin le, le midi, le soir ou n'importe quelle moment de la journée");
       
   }  
 } 
-)
+);
 
 client.on("message",message => {
   if(message.author.bot) return;
@@ -121,18 +118,130 @@ client.on("message",message => {
       
   }  
 } 
-)
+);
 
 client.on("message",message => {
   if(message.author.bot) return;
 
   //!oh
   if(message.content == prefix + "oh"){
-      message.reply("que se passe t'il ?");
-      
+      message.reply("que se passe t'il ?");   
   }  
-} 
-)
+});
+
+
+
+
+client.on("message",message => {
+  if(message.author.bot) return;
+  if(message.channel.type == "dm") return;
+  
+  if(message.member.hasPermission("ADMINISTRATOR")){
+      if(message.content.startsWith(prefix + "ban" )){
+         let mention = message.mentions.members.first();
+
+         if(mention == undefined){
+             message.reply("Membre non ou mal mentionné.")
+         }
+         else{
+             if(mention.bannable){
+                 message.ban();
+                 message.channel.send(mention.displayName + "a été banni désolé pour ça il y'a surement une tres grosse raison")
+
+             }
+             else{
+                message.reply("Impossible de bannir ce membre.");
+                  
+                 
+             }
+         }
+      }
+      else if(message.content.startsWith(prefix + "kick")){
+          let mention = message.mentions.members.first()
+          
+          if(mention == undefined){
+            message.reply("Membre non ou mal mentionné");
+          }
+          else{
+              if(mention.kickable){
+                mention.kick;
+                message.channel.send(mention.displayName + "a été kick pardon pour ça mais il y'a certainement une grosse raison" )
+              }
+              else {
+                  message.reply("Impossible de kick ce membre.");
+              }
+          }
+          
+
+            
+      }
+       else if(message.content.startsWith(prefix + "mute")){
+         let mention = message.mentions.members.first();
+
+         if(mention == undefined){
+          messsage.reply("Membre non ou mal mentionné.");
+         }
+
+         else {
+          mention.roles.add("849341514865442816");
+          message.reply(mention.displayName + "mute avec succes");
+      }
+
+           
+      }
+      
+      
+  }
+  else if(message.content.startsWith(prefix + "unmute")){
+       let mention = message.mentions.members.first();
+
+       if(mention == undefined){
+            message.reply("Membre non ou mal mentionné.");
+         }
+
+         else{
+          mention.roles.add("849341514865442816");
+          message .reply(mention.displayName + "unmute avec succes");
+       }
+
+  }
+  else if(message.content.startsWith(prefix + "tempmute")){
+    let mention = message.mentions.members.first(); 
+
+    if(mention == undefined){
+       message.reply("Membre non ou mal mentionné")
+    }
+    else{
+     let args = message.content.split("");
+
+     mention.roles.add("49341514865442816")
+     setTimeout(function(){
+       mention.roles.remove("49341514865442816")
+       message.channel.send("<@" + mention.id + "> welcome back !");
+
+
+     }, args[2] * 1000 );  
+   }
+
+ }
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 client.on("message",message => {
   if(message.member.permissions.has("MANAGE_MESSAGES")){
